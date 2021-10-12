59566a4c69232bf64ae0c55cfa3c71db2a5007db
PRASHANTH PASHAM <pp741x@att.com>
2021-10-12 14:46:32 -0400

    added routing

--- ATT.PublicCloud.Bastion/ssp-bastion-portal/src/app/app-routing.module.ts
+++ ATT.PublicCloud.Bastion/ssp-bastion-portal/src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { PageLayoutComponent } from './shared/layout/pagelayout/pagelayout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: DashboardComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: './modules/dashboard.module#DashboardModule'
+      }
+    ]
+  },
+  {
+    path: '',
+    component: PageLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: './layout/layout.module#LayoutModule'
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
